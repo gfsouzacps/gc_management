5c3764669e86a1778b7b3d1ccd17c730a44a5f64
Guilherme Souza <91922030+gfsouzacps@users.noreply.github.com>
2023-10-05 21:47:18 -0300

    First Commit

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace gc_management;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace gc_management;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
